d4973d553638fa2f4a025161771e8d0ea03b21e7
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-01-27 16:54:25 +0300

    add LoginUser, login/

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from task_manager.views import IndexPageView
+from task_manager.views import IndexPageView, LoginUser
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexPageView.as_view(), name='index'),
+    path('login/', LoginUser.as_view(), name='login'),
     path('users/', include('task_manager.users.urls')),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,27 @@
+from django import forms
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.views.generic import TemplateView
 
 
 class IndexPageView(TemplateView):
     template_name = "index.html"
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label="Логин",
+                               widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label="Пароль",
+                               widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password']
+
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'login.html'
+    next_page = reverse_lazy("index")
