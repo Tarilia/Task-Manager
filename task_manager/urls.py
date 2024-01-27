d97cb378bb1791a3744a1130c37765efec86cd0b
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-01-27 14:40:21 +0300

    add users

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from task_manager.views import IndexPageView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexPageView.as_view(), name='index'),
+    path('users/', include('task_manager.users.urls')),
 ]
