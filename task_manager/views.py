c86c054e87066d0e4ad18149bde5e95cf8284a2b
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-02-01 16:13:47 +0300

    update messages

--- task_manager/views.py
+++ task_manager/views.py
@@ -15,8 +15,11 @@ class IndexPageView(TemplateView):
 class LoginUserView(LoginView):
     form_class = LoginUserForm
     template_name = 'login.html'
-    success_message = _("You are logged in")
     next_page = reverse_lazy("index")
+
+    def form_valid(self, form):
+        messages.success(self.request, _('You are logged in'))
+        return super().form_valid(form)
 
     def form_invalid(self, form):
         messages.warning(self.request,
