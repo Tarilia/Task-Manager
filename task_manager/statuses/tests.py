7aa96d4e6a435a7374dd510d864b18f1a1c09094
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-02-07 16:45:33 +0300

    add test for statuses

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestStatus(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def index_status(self):
+        response = self.client.get("/statuses/")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse('index_statuses'))
+        self.assertNotContains(response, 'Name status')
