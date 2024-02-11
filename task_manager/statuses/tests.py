a634518e63eb0f38001c6fe74a6ab8e473664aeb
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-02-11 14:26:37 +0300

    add test create status

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -1,14 +1,38 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from task_manager.users.models import User
 
-class TestStatus(TestCase):
+
+class IndexStatus(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def index_status(self):
+    def test_index_status(self):
         response = self.client.get("/statuses/")
         self.assertEqual(response.status_code, 200)
 
         response = self.client.get(reverse('index_statuses'))
         self.assertNotContains(response, 'Name status')
+
+
+class TestCreate(TestCase):
+    fixtures = [
+        'statuses.json',
+        'users.json']
+
+    def setUp(self):
+        self.client = Client()
+        self.client.force_login(User.objects.first())
+
+    def test_statuses_crud(self):
+        response = self.client.get(reverse('index_statuses'))
+        self.assertNotContains(response, 'Another status')
+
+        response = self.client.post(
+            reverse('create_statuses'), data={'name': 'Another status'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'], reverse('index_statuses'))
+
+        response = self.client.get(reverse('index_statuses'))
+        self.assertContains(response, 'Another status')
