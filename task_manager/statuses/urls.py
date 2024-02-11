550076c2c24442cbb55cd2c71446b10e23c19f51
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-02-11 14:56:59 +0300

    add create statuses

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from task_manager.statuses.views import IndexStatusesView
+from task_manager.statuses.views import IndexStatusesView, CreateStatusesView
 
 urlpatterns = [
     path("", IndexStatusesView.as_view(), name="index_statuses"),
+    path("create/", CreateStatusesView.as_view(), name="create_statuses"),
 ]
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,10 +1,22 @@
-from django.contrib.auth import get_user_model
-from django.views.generic import ListView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from django.views.generic.base import ContextMixin
+from django.utils.translation import gettext as _
 
+from task_manager.statuses.forms import CreateStatusesForm
+from task_manager.statuses.models import Status
 from task_manager.utils import AuthRequiredMixin
 
 
 class IndexStatusesView(AuthRequiredMixin, ListView, ContextMixin):
     template_name = "statuses/index.html"
-    model = get_user_model()
+    model = Status
+
+
+class CreateStatusesView(AuthRequiredMixin, SuccessMessageMixin,
+                         CreateView):
+    template_name = "statuses/create.html"
+    form_class = CreateStatusesForm
+    success_url = reverse_lazy("index_statuses")
+    success_message = _("Status successfully created")
