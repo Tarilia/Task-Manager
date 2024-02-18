f1d361d684ecd37a7a3fa11b915342e9bc4257d0
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-02-18 15:03:08 +0300

    add urls, html for label create

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from task_manager.labels.views import IndexLabelsView
+from task_manager.labels.views import IndexLabelsView, CreateLabelsView
 
 
 urlpatterns = [
     path("", IndexLabelsView.as_view(), name="index_labels"),
+    path("create/", CreateLabelsView.as_view(), name="create_labels"),
 ]
