82eca7eafe75ff7c076c947d88f2f8cea3aeba82
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-02-19 14:46:32 +0300

    add views, urls, html for label delete

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from task_manager.labels.views import (IndexLabelsView, CreateLabelsView,
-                                       UpdateLabelsView)
+                                       UpdateLabelsView, DeleteLabelsView)
 
 
 urlpatterns = [
     path("", IndexLabelsView.as_view(), name="index_labels"),
     path("create/", CreateLabelsView.as_view(), name="create_labels"),
     path("<int:pk>/update/", UpdateLabelsView.as_view(), name="update_labels"),
+    path("<int:pk>/delete/", DeleteLabelsView.as_view(), name="delete_labels"),
 ]
--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,12 +1,13 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import (ListView, CreateView, UpdateView,
+                                  DeleteView)
 from django.views.generic.base import ContextMixin
 from django.utils.translation import gettext as _
 
 from task_manager.labels.forms import LabelsForm
 from task_manager.labels.models import Label
-from task_manager.utils import AuthRequiredMixin
+from task_manager.utils import AuthRequiredMixin, ProtectedDeletionMixin
 
 
 class IndexLabelsView(AuthRequiredMixin, ListView, ContextMixin):
@@ -29,3 +30,13 @@ class UpdateLabelsView(AuthRequiredMixin, SuccessMessageMixin,
     form_class = LabelsForm
     success_url = reverse_lazy("index_labels")
     success_message = _("Label changed successfully")
+
+
+class DeleteLabelsView(AuthRequiredMixin, SuccessMessageMixin,
+                       ProtectedDeletionMixin, DeleteView):
+    template_name = "labels/delete.html"
+    model = Label
+    success_message = _("Label successfully deleted")
+    success_url = reverse_lazy("index_labels")
+    protected_message = _('Cannot delete label because it is in use')
+    no_protected_redirect_url = reverse_lazy("index_labels")
