c87322b64ae0731ed83b2a962d2b72abee08936e
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-02-23 15:14:33 +0300

    update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -115,21 +115,13 @@ DATABASES['default'].update(
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#    },
+
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {
             'min_length': 3,
         },
    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#    },
-#    {
-#      'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#    },
 ]
 
 
@@ -140,13 +132,11 @@ AUTH_USER_MODEL = "users.User"
 
 LANGUAGE_CODE = 'ru-RU'
 
-LOCALE_PATHS = (
-os.path.join(BASE_DIR, 'locale/'),
-)
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale/'),)
 
 LANGUAGES = (
-('en', 'English'),
-('ru', 'Russian'),
+    ("en", "English"),
+    ("ru", "Russian"),
 )
 
 TIME_ZONE = 'UTC'
@@ -169,4 +159,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
