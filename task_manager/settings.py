14fcfedaf41d1b69d91347670a68ffe248b21143
Tatiana_Garbuzova <tatiannna0697@gmail.com>
2024-05-01 15:41:53 +0300

    update db

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -102,8 +102,15 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+DATABASES['default'].update(
+    dj_database_url.config(conn_max_age=600)
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
